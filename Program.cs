92bb186eea02063b5c2cfb95e348b0ecef63c01b
JibbityJobbity <37820565+JibbityJobbity@users.noreply.github.com>
2018-03-30 16:04:26 +1300

    Setting up Discord.Net

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace discord_cli
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var client = new DiscordSocketClient();
+            string token = "";
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+    }
+}

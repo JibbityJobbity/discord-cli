2b2f2a30844b0a32ec7ac7ce5291d889f10ce630
JibbityJobbity <37820565+JibbityJobbity@users.noreply.github.com>
2018-03-30 19:21:01 +1300

    Allowing login using tokens and lists the available guilds

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -7,16 +8,52 @@ namespace discord_cli
 {
     class Program
     {
+        SocketChannel currentChannel;
+        DiscordSocketClient client = new DiscordSocketClient();
+
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-            var client = new DiscordSocketClient();
             string token = "";
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
+
+            client.Connected += Connected;
+
+            Console.Write("Please enter your Discord token: ");
+            token = Console.ReadLine();
+            Console.Clear();
+            await client.LoginAsync(TokenType.User, token);
+            Console.Write("Connecting");
+            client.StartAsync();
+            while (client.ConnectionState != ConnectionState.Connected)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+            Console.Clear();
+            Console.WriteLine("Connected");
+
             await Task.Delay(-1);
+        }
+
+        Task Connected()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("Logged in as " + client.CurrentUser.Username + "#" + client.CurrentUser.Discriminator);
+            Console.WriteLine("Type \"help\" or \"?\" for a list of commands");
+            switch (Console.ReadLine())
+            {
+                case "list":
+                    int i = 1;
+                    foreach (SocketGuild guild in client.Guilds)
+                    {
+                        Console.WriteLine("[" + i + "] " + guild.Name);
+                        i++;
+                    }
+                    break;
+            }
+            return Task.CompletedTask;
         }
     }
 }
